36cab1135e11532c4a41e76993c3de711e4a6c82
Erkan <erkankolakan@gmail.com>
2023-08-14 16:11:59 +0300

    baslangıc

---
+++ src/app/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <header>
+        <body>
+            {children}
+        </body>
+    </header>
+  )
+}
+
+export default layout
